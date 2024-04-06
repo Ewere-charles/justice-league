b3518712fcaf30ec30e911a7a0c2d3dac5ba362e
Ewere-charles <152030763+Ewere-charles@users.noreply.github.com>
2024-04-06 23:45:35 +0100

    Delete DC_web directory

--- DC_web/js/index.js
+++
@@ -1,7 +0,0 @@
-const socialLinks = document.querySelector('.socials');
-const collapseMenu = document.querySelector('.collapse_menu');
-
-socialLinks.addEventListener('click', ()=>{
-    collapseMenu.classList.toggle('collapse');
-})
-
